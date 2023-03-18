20b30a566dc9e960a13157c94255cf410f77f648
koonsland <nicegsp@gmail.com>
2023-03-18 10:53:55 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/com/koon/blogsearchservice/BlogSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.koon.blogsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSearchServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/koon/blogsearchservice/BlogSearchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koon.blogsearchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogSearchServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
